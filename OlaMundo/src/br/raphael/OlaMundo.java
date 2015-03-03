8e389a0b60203b59932eb270a2216fb1832815c0
RACMAL <RACMAL@10.12.27.110>
2015-03-02 22:28:04 -0300

    Aula 01

---
+++ OlaMundo/src/br/raphael/OlaMundo.java
@@ -0,0 +1,32 @@
+package br.raphael;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + " Olá Mundo");	
+		}
+		/*int i = 0;
+		while (i < 10) {
+			System.out.println(i + 1 + " Olá Mundo");
+			i++;
+		}*/
+	
+	Aluno a1 = new Aluno(11316789L, "Maria Filisbina", "12345678", new Date());
+	Aluno a2 = new Aluno(9813045L, "Golaço merece placa", "04395485890");
+	Aluno a3 = new Aluno(9813045L, "Golaço merece placa");
+	
+	Professor p1 = new Professor("Humberto Cruvinel", "982354875", new BigDecimal("9587"));
+	Professor p2 = new Professor("Humberto Cruvinel", "982354875");
+	
+	System.out.println(a1);
+	System.out.println(a2);
+	System.out.println(a3);
+	System.out.println(p1);
+	System.out.println(p2);
+	}
+}
---
+++ OlaMundo/src/br/raphael/Professor.java
@@ -0,0 +1,31 @@
+package br.raphael;
+
+import java.math.BigDecimal;
+
+public class Professor extends Pessoa {
+
+	private BigDecimal salario;
+
+	public Professor(String nome, String cpf, BigDecimal salario) {
+		super(nome, cpf);
+		this.salario = salario;
+	}
+
+	public Professor(String nome, String cpf) {
+		super(nome, cpf);
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Professor [salario=" + salario + "]";
+	}
+
+}
